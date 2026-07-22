3bd9b10df371a019876e7da516823f57270287d0
nighthaw1k <parthsantoshsingh098@gmail.com>
2026-07-23 01:17:48 +0530

    js file added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const board = document.querySelector(".board");
 const blockheight = 30;
 const blockwidth = 30;
+
+const cols = Math.floor(board.clientWidth / blockwidth);
+const rows = Math.floor(board.clientHeight / blockheight);
+
+for (let i = 0; i < rows * cols; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+}
