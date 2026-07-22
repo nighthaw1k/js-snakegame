7b5b5d09402ea5519346f8aff11d35bbe55feeba
nighthaw1k <parthsantoshsingh098@gmail.com>
2026-07-23 01:37:13 +0530

    Updation

--- script.js
+++ script.js
@@ -5,8 +5,26 @@ const blockwidth = 30;
 const cols = Math.floor(board.clientWidth / blockwidth);
 const rows = Math.floor(board.clientHeight / blockheight);
 
-for (let i = 0; i < rows * cols; i++) {
-    const block = document.createElement("div");
-    block.classList.add("block");
-    board.appendChild(block);
+const blocks = [];
+const snake = [{ x: 1, y: 3 }, { x: 1, y: 2 }, { x: 1, y: 1 }];
+
+let direction = "right";
+
+
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        const block = document.createElement("div");
+        block.classList.add("block");
+        board.appendChild(block);
+        block.innerText = `${i},${j}`;
+        blocks[`${i},${j}`] = block;
+    }
 }
+
+function render() {
+    snake.forEach((block) => {
+        blocks[`${block.x},${block.y}`].classList.add("fill");
+    });
+}
+
+
