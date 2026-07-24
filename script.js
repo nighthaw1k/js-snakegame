8dd7f9cb56b42d25a3107b69dd67bc9c845cade5
nighthaw1k <parthsantoshsingh098@gmail.com>
2026-07-24 12:54:11 +0530

    Updation

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const blockwidth = 30;
 
 const cols = Math.floor(board.clientWidth / blockwidth);
 const rows = Math.floor(board.clientHeight / blockheight);
+let intervalId = null;
 
 const blocks = [];
 const snake = [{ x: 1, y: 3 }, { x: 1, y: 2 }, { x: 1, y: 1 }];
@@ -26,5 +27,49 @@ function render() {
         blocks[`${block.x},${block.y}`].classList.add("fill");
     });
 }
+
+intervalId = setInterval(() => {
+
+    let head = null
+
+    if (direction === "left") {
+        head = { x: snake[0].x, y: snake[0].y - 1 };
+    } else if (direction === "right") {
+        head = { x: snake[0].x, y: snake[0].y + 1 };
+    } else if (direction === "up") {
+        head = { x: snake[0].x - 1, y: snake[0].y };
+    } else if (direction === "down") {
+        head = { x: snake[0].x + 1, y: snake[0].y };
+    }
+
+    if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
+        alert("Game Over");
+        snake.length = 0;
+        clearInterval(intervalId);
+    }
+
+    snake.forEach((block) => {
+        blocks[`${block.x},${block.y}`].classList.remove("fill");
+    })
+
+    snake.unshift(head);
+    snake.pop();
+
+    render();
+}, 400);
+
+    addEventListener("keydown", (e) => {
+        if (e.key === "ArrowLeft") {
+            direction = "left";
+        } else if (e.key === "ArrowRight") {
+            direction = "right";
+        } else if (e.key === "ArrowUp") {
+            direction = "up";
+        } else if (e.key === "ArrowDown") {
+            direction = "down";
+        }
+    })
+
+    
 
 
